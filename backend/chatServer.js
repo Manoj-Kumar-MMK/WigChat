62bb273035f9f991c37383585b7fbbbd34c81820
th3h04x <56202570+blessingcharles@users.noreply.github.com>
2021-05-19 12:30:14 +0000

    Add files via upload

--- backend/chatServer.js
+++ backend/chatServer.js
@@ -45,6 +45,27 @@ db.connect(function(err) {
 
 });
 
+const handleDisconnect = ()=>{
+
+    db.on('error', function (error) {
+        if (!error.fatal) return;
+        if (error.code !== 'PROTOCOL_CONNECTION_LOST') throw err;
+
+        console.error('>[+] Re-connecting lost MySQL connection: ');
+
+        db = mysql.createConnection(db_connect);
+        handleDisconnect();
+        db.connect((err)=>{
+
+            if(err) console.log("[-]reconnection also failed ");
+
+        });
+    });
+
+}
+
+handleDisconnect();
+
 // server.listen(3000,()=>{
 //     console.log("server started");
 
